3db56011af939765268c068391657971d58da5ce
Teodor Savin <teodor@aceandtate.com>
2017-02-20 00:58:00 +0100

    add old function in blade templates

--- resources/views/information/create.blade.php
+++ resources/views/information/create.blade.php
@@ -19,80 +19,80 @@
                     <div class="form-group">
                         <label for="first-name" class="col-lg-2 control-label">First Name</label>
                         <div class="col-lg-10">
-                            <input type="text" class="form-control" id="first-name" placeholder="First name" name="first_name">
+                            <input type="text" class="form-control" id="first-name" placeholder="First name" name="first_name" value="{{ old('first_name') }}">
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="last-name" class="col-lg-2 control-label">Last Name</label>
                         <div class="col-lg-10">
-                            <input type="text" class="form-control" id="last-name" placeholder="Last name" name="last_name">
+                            <input type="text" class="form-control" id="last-name" placeholder="Last name" name="last_name" value="{{ old('last_name') }}">
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="bsn" class="col-lg-2 control-label">BSN</label>
                         <div class="col-lg-10">
-                            <input type="text" class="form-control" id="bsn" placeholder="BSN" name="bsn">
+                            <input type="text" class="form-control" id="bsn" placeholder="BSN" name="bsn" value="{{ old('bsn') }}">
                             <span class="help-block">Don't worry! This will be encrypted!</span>
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="email" class="col-lg-2 control-label">E-mail</label>
                         <div class="col-lg-10">
-                            <input type="email" class="form-control" id="email" placeholder="E-mail" name="email">
+                            <input type="email" class="form-control" id="email" placeholder="E-mail" name="email" value="{{ old('email') }}">
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="phone" class="col-lg-2 control-label">Phone number</label>
                         <div class="col-lg-10">
-                            <input type="text" class="form-control" id="phone" placeholder="Phone number" name="phone">
+                            <input type="text" class="form-control" id="phone" placeholder="Phone number" name="phone" value="{{ old('phone') }}">
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="street" class="col-lg-2 control-label">Street</label>
                         <div class="col-lg-10">
-                            <input type="text" class="form-control" id="street" placeholder="The Hitchhiker's Guide to the Galaxy" name="street">
+                            <input type="text" class="form-control" id="street" placeholder="The Hitchhiker's Guide to the Galaxy" name="street" value="{{ old('street') }}">
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="house-number" class="col-lg-2 control-label">House Number</label>
                         <div class="col-lg-10">
-                            <input type="text" class="form-control" id="house-number" placeholder="42" name="house_number">
+                            <input type="text" class="form-control" id="house-number" placeholder="42" name="house_number" value="{{ old('house_number') }}">
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="postal-code" class="col-lg-2 control-label">Postal code</label>
                         <div class="col-lg-10">
-                            <input type="text" class="form-control" id="postal-code" placeholder="1019BW" name="postal_code">
+                            <input type="text" class="form-control" id="postal-code" placeholder="1019BW" name="postal_code" value="{{ old('postal_code') }}">
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="city" class="col-lg-2 control-label">City</label>
                         <div class="col-lg-10">
-                            <input type="text" class="form-control" id="city" placeholder="Gotham City" name="city">
+                            <input type="text" class="form-control" id="city" placeholder="Gotham City" name="city" value="{{ old('city') }}">
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="country" class="col-lg-2 control-label">Country</label>
                         <div class="col-lg-10">
-                            <input type="text" class="form-control" id="country" placeholder="Neverland" name="country">
+                            <input type="text" class="form-control" id="country" placeholder="Neverland" name="country" value="{{ old('country') }}">
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="iban" class="col-lg-2 control-label">IBAN</label>
                         <div class="col-lg-10">
-                            <input type="text" class="form-control" id="iban" placeholder="NL12ABNA1234567890" name="iban">
+                            <input type="text" class="form-control" id="iban" placeholder="NL12ABNA1234567890" name="iban" value="{{ old('iban') }}">
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="credit-card-number" class="col-lg-2 control-label">Credit Card</label>
                         <div class="col-lg-10">
-                            <input type="text" class="form-control" id="credit-card-number" placeholder="1234 5432 6789 9876" name="credit_card_number">
+                            <input type="text" class="form-control" id="credit-card-number" placeholder="1234 5432 6789 9876" name="credit_card_number" value="{{ old('credit_card_number') }}">
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="civ" class="col-lg-2 control-label">CIV</label>
                         <div class="col-lg-10">
-                            <input type="text" class="form-control" id="civ" placeholder="123" name="civ">
+                            <input type="text" class="form-control" id="civ" placeholder="123" name="civ" value="{{ old('civ') }}">
                         </div>
                     </div>
                     <div class="form-group">
--- resources/views/information/edit.blade.php
+++ resources/views/information/edit.blade.php
@@ -19,80 +19,80 @@
                     <div class="form-group">
                         <label for="first-name" class="col-lg-2 control-label">First Name</label>
                         <div class="col-lg-10">
-                            <input type="text" class="form-control" id="first-name" placeholder="First name" name="first_name" value="{!! $information->first_name !!}">
+                            <input type="text" class="form-control" id="first-name" placeholder="First name" name="first_name" value="{!! old('first_name') ?? $information->first_name !!}">
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="last-name" class="col-lg-2 control-label">Last Name</label>
                         <div class="col-lg-10">
-                            <input type="text" class="form-control" id="last-name" placeholder="Last name" name="last_name" value="{!! $information->last_name !!}">
+                            <input type="text" class="form-control" id="last-name" placeholder="Last name" name="last_name" value="{!! old('last_name') ?? $information->last_name !!}">
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="bsn" class="col-lg-2 control-label">BSN</label>
                         <div class="col-lg-10">
-                            <input type="text" class="form-control" id="bsn" placeholder="BSN" name="bsn" value="{!! $information->bsn !!}">
+                            <input type="text" class="form-control" id="bsn" placeholder="BSN" name="bsn" value="{!! old('bsn') ?? $information->bsn !!}">
                             <span class="help-block">Don't worry! This will be encrypted!</span>
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="email" class="col-lg-2 control-label">E-mail</label>
                         <div class="col-lg-10">
-                            <input type="email" class="form-control" id="email" placeholder="E-mail" name="email" value="{!! $information->email !!}">
+                            <input type="email" class="form-control" id="email" placeholder="E-mail" name="email" value="{!! old('email') ?? $information->email !!}">
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="phone" class="col-lg-2 control-label">Phone number</label>
                         <div class="col-lg-10">
-                            <input type="text" class="form-control" id="phone" placeholder="Phone number" name="phone" value="{!! $information->phone !!}">
+                            <input type="text" class="form-control" id="phone" placeholder="Phone number" name="phone" value="{!! old('phone') ?? $information->phone !!}">
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="street" class="col-lg-2 control-label">Street</label>
                         <div class="col-lg-10">
-                            <input type="text" class="form-control" id="street" placeholder="The Hitchhiker's Guide to the Galaxy" name="street" value="{!! $information->street !!}">
+                            <input type="text" class="form-control" id="street" placeholder="The Hitchhiker's Guide to the Galaxy" name="street" value="{!! old('street') ?? $information->street !!}">
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="house-number" class="col-lg-2 control-label">House Number</label>
                         <div class="col-lg-10">
-                            <input type="text" class="form-control" id="house-number" placeholder="42" name="house_number" value="{!! $information->house_number !!}">
+                            <input type="text" class="form-control" id="house-number" placeholder="42" name="house_number" value="{!! old('house_number') ?? $information->house_number !!}">
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="postal-code" class="col-lg-2 control-label">Postal code</label>
                         <div class="col-lg-10">
-                            <input type="text" class="form-control" id="postal-code" placeholder="1019BW" name="postal_code" value="{!! $information->postal_code !!}">
+                            <input type="text" class="form-control" id="postal-code" placeholder="1019BW" name="postal_code" value="{!! old('postal_code') ?? $information->postal_code !!}">
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="city" class="col-lg-2 control-label">City</label>
                         <div class="col-lg-10">
-                            <input type="text" class="form-control" id="city" placeholder="Gotham City" name="city" value="{!! $information->city !!}">
+                            <input type="text" class="form-control" id="city" placeholder="Gotham City" name="city" value="{!! old('city') ?? $information->city !!}">
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="country" class="col-lg-2 control-label">Country</label>
                         <div class="col-lg-10">
-                            <input type="text" class="form-control" id="country" placeholder="Neverland" name="country" value="{!! $information->country !!}">
+                            <input type="text" class="form-control" id="country" placeholder="Neverland" name="country" value="{!! old('country') ?? $information->country !!}">
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="iban" class="col-lg-2 control-label">IBAN</label>
                         <div class="col-lg-10">
-                            <input type="text" class="form-control" id="iban" placeholder="NL12ABNA1234567890" name="iban" value="{!! $information->IBAN !!}">
+                            <input type="text" class="form-control" id="iban" placeholder="NL12ABNA1234567890" name="iban" value="{!! old('iban') ?? $information->IBAN !!}">
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="credit-card-number" class="col-lg-2 control-label">Credit Card</label>
                         <div class="col-lg-10">
-                            <input type="text" class="form-control" id="credit-card-number" placeholder="1234 5432 6789 9876" name="credit_card_number" value="{!! $information->credit_card_number !!}">
+                            <input type="text" class="form-control" id="credit-card-number" placeholder="1234 5432 6789 9876" name="credit_card_number" value="{!! old('credit_card_number') ?? $information->credit_card_number !!}">
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="civ" class="col-lg-2 control-label">CIV</label>
                         <div class="col-lg-10">
-                            <input type="text" class="form-control" id="civ" placeholder="123" name="civ" value="{!! $information->civ !!}">
+                            <input type="text" class="form-control" id="civ" placeholder="123" name="civ" value="{!! old('civ') ?? $information->civ !!}">
                         </div>
                     </div>
                     <div class="form-group">
